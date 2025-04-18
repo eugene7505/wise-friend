84d78ca22b9146ff97c0da52760f0714ba900edf
Eugene <eugene7505@gmail.com>
2025-04-18 16:40:25 -0700

    config

--- config.py
+++ config.py
@@ -4,9 +4,7 @@
 # https://fireworks.ai/models/fireworks/llama-v3p1-8b-instruct
 CHAT_MODEL = "accounts/fireworks/models/llama-v3p1-8b-instruct"
 EMBEDDINGS_MODEL = "nomic-ai/nomic-embed-text-v1.5"
-WISE_COLLECTION = "wise_friend"
-JOURNAL_COLLECTION = "journal"
-WISE_COLLECTION_TABLE = "collection_table"
+WISE_COLLECTION_TABLE = "wise_collection"
 
 # Prompt related variables
 PROMPT = "randomguy/rag_with_chat_history_and_citation"  # Original: "rlm/rag-prompt"
