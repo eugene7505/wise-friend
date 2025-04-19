14448e3f80689bcc4ab02a1d9c750af3d0708e72
Eugene <eugene7505@gmail.com>
2025-04-18 22:42:16 -0700

    url

--- wise_friend_streamlit.py
+++ wise_friend_streamlit.py
@@ -144,7 +144,7 @@ authenticator = Authenticate(
     secret_credentials_path="client_secret.json",
     cookie_name="my_cookie_name",  # TODO: do we need this?
     cookie_key="this_is_secret",  # TODO: do we need this?
-    redirect_uri="http://localhost:8501",
+    redirect_uri="https://wise-friend-dev.streamlit.app",
 )
 
 ### Streamlit interface
